9597fe5ede5b9efaf4c8ec7b63cf16beafea1211
Erik Hofer <erik-hofer@mail.de>
2017-05-03 19:45:50 +0200

    Added temporary fix for https://github.com/tbroyer/gradle-apt-plugin/issues/49

---
+++ src/test/groovy/com/xinra/nucleus/service/TestDtoImpl.java
@@ -0,0 +1,24 @@
+package com.xinra.nucleus.service;
+
+public class TestDtoImpl extends DtoImpl implements TestDto {
+  
+  private int foo;
+  private boolean bar;
+  
+  
+  public int getFoo() {
+    return foo;
+  }
+  public void setFoo(int foo) {
+    this.foo = foo;
+  }
+  public boolean getBar() {
+    return bar;
+  }
+  public boolean isBar() {
+    return bar;
+  }
+  public void setBar(boolean bar) {
+    this.bar = bar;
+  }
+}
--- src/test/groovy/com/xinra/nucleus/service/TestDtoImpl2.groovy
+++ src/test/groovy/com/xinra/nucleus/service/TestDtoImpl2.groovy
@@ -4,8 +4,12 @@ import com.xinra.nucleus.service.DtoImpl
 import com.xinra.nucleus.service.TestDto
 import groovy.transform.CompileStatic
 
+/**
+ * Annotation processing for groovy tests is currently broken.
+ * See https://github.com/tbroyer/gradle-apt-plugin/issues/49
+ */
 @CompileStatic
-class TestDtoImpl extends DtoImpl implements TestDto {
+class TestDtoImpl2 extends DtoImpl implements TestDto {
   
   def int foo;
   def boolean bar;
